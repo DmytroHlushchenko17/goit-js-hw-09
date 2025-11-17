e646e2412ffde270986c0c4b1fa548407f7af789
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2025-11-17 12:16:46 +0200

    hw-9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,23 +1,23 @@
 const feedbackForm = document.querySelector('.js-feedback-form');
 
-let formDate = {
+let formData = {
   email: '',
   message: '',
 };
 
 const fillFeedbackFormFields = () => {
-  const formDateFromLS = JSON.parse(
+  const formDataFromLS = JSON.parse(
     localStorage.getItem('feedback-form-state')
   );
   if (formDateFromLS === null) {
     return;
   }
 
-  formDate = formDateFromLS;
+  formData = formDataFromLS;
 
-  const formDataFromLSKeys = Object.keys(formDateFromLS);
+  const formDataFromLSKeys = Object.keys(formDataFromLS);
   formDataFromLSKeys.forEach(key => {
-    feedbackForm.elements[key].value = formDateFromLS[key];
+    feedbackForm.elements[key].value = formDataFromLS[key];
   });
 };
 
@@ -26,14 +26,14 @@ const onFeedbackFormInput = event => {
   const formFieldValue = formField.value.trim();
   const formFieldName = formField.name;
 
-  formDate[formFieldName] = formFieldValue;
+  formData[formFieldName] = formFieldValue;
 
   localStorage.setItem('feedback-form-state', JSON.stringify(formDate));
 };
 
 const onFeedbackFormSudmit = event => {
   event.preventDefault();
-  if (formDate.email === '' || formDate.message === '') {
+  if (formData.email === '' || formData.message === '') {
     alert('«Fill please all fields»');
     return;
   }
